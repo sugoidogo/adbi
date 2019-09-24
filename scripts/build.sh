a62f1e998f6cfde5b826a895f92990849de22934
Joseph mendoza <josephsmendoza@gmail.com>
2019-09-23 23:58:59 -0700

    Delete build.sh

--- scripts/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cd $(dirname $BASH_SOURCE)/..
-dub build --single src/adbi.d 
-dub build --single src/common.d
-upx --best bin/linux/x86_64/adbi
-upx --best bin/linux/x86_64/common
-mksiofs -o bin/linux.iso bin/linux/x86_64
-cat res/stub.sh bin/linux.iso > adbi.d
